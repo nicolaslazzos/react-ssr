4c00d209b42f952ebea68e7e0b8950e23be60534
nicolaslazzos <nicolaslazzos09@gmail.com>
2020-07-16 12:12:44 -0300

    setting up routing on both server and client side

--- src/client/client.js
+++ src/client/client.js
@@ -1,9 +1,12 @@
 // startup point for the client side application
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './components/Home';
+import Routes from './Routes';
+import { BrowserRouter } from 'react-router-dom';
 
 // this is gonna hydrate the html content previously rendered in the server and sent to the client
 // it means that is not going to re render the content and replace it, its going to set the event
 // handlers and all the necessary things to make work the app
-ReactDOM.hydrate(<Home />, document.querySelector('#root'));
+
+// we use BrowserRouter because this handles the client side routing
+ReactDOM.hydrate(<BrowserRouter><Routes /></BrowserRouter>, document.querySelector('#root'));
